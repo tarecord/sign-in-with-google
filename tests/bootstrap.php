6ae13167ca811b57c670338627795a62dc421527
Tanner Record <tanner.record@gmail.com>
2018-01-10 17:44:06 -0500

    update wp-cli scaffolding of plugin tests to version 1.1.1

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,17 @@
 /**
  * PHPUnit bootstrap file
  *
- * @package Google_Sign_In
+ * @package Google_Sign_Up
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	throw new Exception( "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" );
 }
 
 // Give access to tests_add_filter() function.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Google_Sign_Up
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
