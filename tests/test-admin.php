c47eb46193dfa0fe06df62600ce0d57e054604a0
Tanner Record <tanner.record@gmail.com>
2018-01-29 14:18:26 -0500

    Refactor domain input test to use @dataProvider

--- tests/test-admin.php
+++ tests/test-admin.php
@@ -35,24 +35,23 @@ class AdminTest extends WP_UnitTestCase {
 	/**
 	 * Tests the domain input validation. Users must provide a correct domain or comma
 	 * delimited string of domains.
+	 * 
+	 * @dataProvider domain_test_array
 	 */
-	function test_verify_domain_list() {
-
+	function test_verify_domain_list($input, $expected) {
 		$google_sign_in_admin = new Google_Sign_In_Admin( $this->plugin_name, $this->version );
+		$result = $google_sign_in_admin->verify_domain_list( $input );
+		$this->assertEquals( $result, $expected );
+	}
 
-		$inputs = array(
-			'thisisatest.com'                     => true,
-			'thisisatest.com, another.com'        => true,
-			'test.com, another.com, a.com, 1.com' => true,
-			't.com, 1.com, 2.com, 3'              => false,
-			'test.com, another, test.com'         => false,
-			'somedomain, another'                 => false,
-		);
-
-		foreach ( $inputs as $input => $expected ) {
-			$result = $google_sign_in_admin->verify_domain_list( $input );
-			$this->assertEquals( $result, $expected );
-		}
-
+	function domain_test_array() {
+		return [
+			['thisisatest.com', true],
+			['thisisatest.com, another.com', true],
+			['test.com, another.com, a.com, 1.com', true],
+			['t.com, 1.com, 2.com, 3', false],
+			['test.com, another, test.com', false],
+			['somedomain, another', false],
+		];
 	}
 }
