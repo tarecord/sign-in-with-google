ff18cb65928b475e7e4cd526302c3c0b0ef74ed4
Tanner Record <tanner.record@gmail.com>
2020-06-15 10:30:19 -0400

    Update text domain in plugin header

--- src/sign-in-with-google/sign-in-with-google.php
+++ src/sign-in-with-google/sign-in-with-google.php
@@ -20,7 +20,7 @@
  * Author URI:        https://profiles.wordpress.org/northstarmarketing
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       siwg
+ * Text Domain:       sign-in-with-google
  * Domain Path:       /languages
  */
 
