91db8c40e861022ce08dc48bb59b5948e730e70e
Tanner Record <tanner.record@gmail.com>
2020-09-01 16:00:52 -0400

    Fix Travis PHPUnit (#51)
    * Reset to what wp-scaffold tests uses
    
    * Use correct package name

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,17 +1,30 @@
 <?php
 /**
- * Sets up the test suite for PHPUnit.
+ * PHPUnit bootstrap file
  *
  * @package Sign_In_With_Google
  */
 
-require '/wp-phpunit/includes/functions.php';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-tests_add_filter(
-	'muplugins_loaded',
-	function () {
-		require dirname( __FILE__ ) . '/../src/sign-in-with-google/sign-in-with-google.php';
-	}
-);
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
 
-require '/wp-phpunit/includes/bootstrap.php';
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	throw new Exception( "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" );
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/src/sign-in-with-google/sign-in-with-google.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
