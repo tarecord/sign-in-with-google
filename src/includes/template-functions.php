9ba95f89001c0513c204500fb8c0626106ea003f
T.T <7117978+ttodua@users.noreply.github.com>
2022-01-28 22:31:40 +0400

    global function

--- src/includes/template-functions.php
+++ src/includes/template-functions.php
@@ -25,3 +25,13 @@ function siwg_button() {
 function siwg_get_button() {
 	return Sign_In_With_Google_Public::get_signin_button();
 }
+
+/**
+ * Get the google auth url (for headless usage)
+ *
+ * @return string
+ */
+function siwg_get_google_auth_url( $state = [] ) {
+	$google_auth = new SIWG_GoogleAuth( get_option( 'siwg_google_client_id' ) );
+	return $google_auth->get_google_auth_url( $state );
+}
