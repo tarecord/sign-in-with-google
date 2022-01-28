df8c62b87eeaab8019cd77a42c2fb0ee7d5d8605
T. Todua <7117978+ttodua@users.noreply.github.com>
2022-01-28 22:20:30 +0400

    Update class-sign-in-with-google.php

--- src/includes/class-sign-in-with-google.php
+++ src/includes/class-sign-in-with-google.php
@@ -80,7 +80,6 @@ class Sign_In_With_Google {
 		if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			$this->register_cli_commands();
 		}
-		add_filter( 'siwg_instance', function() { return $this; } );
 	}
 
 	/**
@@ -175,9 +174,9 @@ class Sign_In_With_Google {
 	private function define_admin_hooks() {
 
 		$plugin_admin = new Sign_In_With_Google_Admin( $this->get_plugin_name(), $this->get_version() );
-		add_filter( 'siwg_admin_instance', function() use ($plugin_admin) { return $plugin_admin; } );
 
-		$this->loader->add_action( 'admin_init', $plugin_admin, 'settings_api_init' );		$this->loader->add_action( 'admin_menu', $plugin_admin, 'settings_menu_init' );
+		$this->loader->add_action( 'admin_init', $plugin_admin, 'settings_api_init' );		
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'settings_menu_init' );
 		$this->loader->add_action( 'admin_init', $plugin_admin, 'process_settings_export' );
 		$this->loader->add_action( 'admin_init', $plugin_admin, 'process_settings_import' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
