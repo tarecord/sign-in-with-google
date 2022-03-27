d2f4f46522888630cf37200ff72acdcce98cf002
T. Todua <7117978+ttodua@users.noreply.github.com>
2022-03-28 00:23:29 +0400

    Add global function to get authentication URL (#91)
    * global function
    
    * Update template-functions.php
    
    * Refactor to introduce an early exit w/no client ID
    
    Co-authored-by: Tanner Record <tanner.record@gmail.com>

--- src/includes/template-functions.php
+++ src/includes/template-functions.php
@@ -25,3 +25,23 @@ function siwg_button() {
 function siwg_get_button() {
 	return Sign_In_With_Google_Public::get_signin_button();
 }
+
+/**
+ * Get the Google authentication URL.
+ *
+ * @since [NEXT]
+ *
+ * @param array $state Nonce to verify response from Google.
+ *
+ * @return string
+ */
+function siwg_get_google_auth_url( $state = array() ) {
+	$client_id = get_option( 'siwg_google_client_id' );
+
+	// Bail if there is no client ID.
+	if ( ! $client_id ) {
+		return '';
+	}
+
+	return ( new SIWG_GoogleAuth( $client_id ) )->get_google_auth_url( $state );
+}
