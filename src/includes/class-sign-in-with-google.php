33e62fb64b45a1eef1da9cdd9d745e9fe214d353
Chris Kindred <cmkindred@gmail.com>
2022-04-26 21:33:05 -0400

    Adds Debug Infromation to Health Status Info Page

--- src/includes/class-sign-in-with-google.php
+++ src/includes/class-sign-in-with-google.php
@@ -181,6 +181,7 @@ class Sign_In_With_Google {
 		$this->loader->add_action( 'admin_init', $plugin_admin, 'process_settings_import' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'show_user_profile', $plugin_admin, 'add_connect_button_to_profile' );
+		$this->loader->add_filter( 'debug_information', $plugin_admin, 'add_debug_info_to_site_health', PHP_INT_MAX, 1 );
 
 		if ( isset( $_POST['_siwg_account_nonce'] ) ) {
 			$this->loader->add_action( 'admin_init', $plugin_admin, 'disconnect_account' );
